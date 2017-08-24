710f5df3d80ba5e11d4aefc4293ff917794953f2
Dhiraj Das <dhiraj.das.05@gmail.com>
2017-08-24 17:39:18 +0530

    added gradle project initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button exitButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) { }
+
+    public void onExitPressed(ActionEvent event) {
+        System.exit(0);
+    }
+}
